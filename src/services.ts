7fe2f7b170afcf456edee9e75fb4c4f56652cbdc
Cangjier <moodlee@qq.com>
2024-12-08 04:30:17 +0000

    optimize

--- src/services.ts
+++ src/services.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export class Services {
+    public static FormatUrl (url:string){
+        return `http://localhost:12332${url}`
+    }
     public static GetApplicationInfomation() {
         if ((window as any).webapplication) {
             return (window as any).webapplication as {
@@ -12,28 +15,28 @@ export class Services {
         }
     }
     public static async Exit() {
-        await axios.get("/api/v1/app/exit");
+        await axios.get(Services.FormatUrl("/api/v1/app/exit"));
     }
     public static async Maximize() {
         let webapplication = Services.GetApplicationInfomation();
         if (webapplication) {
-            await axios.get(`/api/v1/app/maximize/${webapplication.id}`);
+            await axios.get(Services.FormatUrl(`/api/v1/app/maximize/${webapplication.id}`));
         }
     }
     public static async Minimize() {
         let webapplication = Services.GetApplicationInfomation();
         if (webapplication) {
-            await axios.get(`/api/v1/app/minimize/${webapplication.id}`);
+            await axios.get(Services.FormatUrl(`/api/v1/app/minimize/${webapplication.id}`));
         }
     }
     public static async Open(url: string) {
         let webapplication = Services.GetApplicationInfomation();
         if (webapplication) {
-            await axios.get(`/api/v1/app/open/${url}`);
+            await axios.get(Services.FormatUrl(`/api/v1/app/open/${url}`));
         }
     }
 
     public static async Home() {
-        await axios.get(`/api/v1/app/home`);
+        await axios.get(Services.FormatUrl(`/api/v1/app/home`));
     }
 }
